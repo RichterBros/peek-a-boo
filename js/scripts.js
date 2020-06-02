3c2aa66a030d3094807a617584edf68cbb161295
Erich Richter <erich.richter@gmail.com>
2020-06-02 11:08:23 -0700

    add effects with jquery > > co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
         $("#walrus-showing").toggle().slideDown();
         $("#walrus-hidden").toggle();
       });
-});
+      
+      $("button#green").click(function() {
+        $("body").removeClass();
+        $("body").addClass("green-background");
+      });
+    
+      $("button#yellow").click(function() {
+        $("body").removeClass();
+        $("body").addClass("yellow-background");
+      });
+    
+      $("button#red").click(function() {
+        $("test").removeClass();
+        $("body").addClass("red-background");
+      });
+
+
+
+
+    });
